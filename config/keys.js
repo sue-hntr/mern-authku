3242f2dccaf6f5fe1f18c1ec65cfedb937ff3e91
Suzanne Hunter <sue_huntr@yahoo.com>
2019-05-19 13:57:22 -0400

    push before build

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+//DEPRECATED DUE TO MONGODB ATLAS ISSUES
+//REPLACED WITH HEROKU PROD mLAB
+//put the mLAB stuff below
+
+module.exports = {
+    mongoURI: "mongodb://heroku_p42wv8z0:heroku_p42wv8z0@ds157946.mlab.com:57946/heroku_p42wv8z0",
+    secretOrKey: "secret"
+  };
---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+require('dotenv').config();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+
+const app = express();
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+
+app.use(bodyParser.json());
+// DB Config
+// const db = require("./config/keys").mongoURI;
+
+// MongoDB for Heroku Prod
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mern", { useNewUrlParser: true })
+// MongoDB for local
+// mongoose.connect("mongodb://localhost/mern", { useNewUrlParser: true })
+.then(() => console.log("MongoDB successfully connected"))
+.catch( err => console.log(err));
+
+//////couldn't get MongoDB Atlas to work within an hour
+// Connect to MongoDB
+// mongoose
+//   .connect(
+//     process.env.MONGODB_URI, { useNewUrlParser: true }
+//   )
+//   .then(() => console.log("MongoDB successfully connected"))
+//   .catch(err => 
+//     console.log(err)
+//     );
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
+
+
+
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
